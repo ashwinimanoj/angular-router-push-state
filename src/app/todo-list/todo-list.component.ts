c1716fdf2dec2abdd7736e937671bd5e39302f58
Ashwini Manoj <ashwini.manoj@thoughtworks.com>
2019-03-24 15:47:50 +0530

    Add TodoList Component

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  todos = [
+    {
+      name: 'Todo 1',
+      done: false
+    },
+    {
+      name: 'Todo 2',
+      done: false
+    },
+    {
+      name: 'Todo 3',
+      done: true
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
