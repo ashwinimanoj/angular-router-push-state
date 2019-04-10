baf5b5b6a7a61c953b9805cce8587e389ac03be2
Ashwini Manoj <ashwini.manoj@thoughtworks.com>
2019-04-10 17:31:30 +0530

    Add Home and PageNotFound component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   {
@@ -17,13 +19,21 @@ const appRoutes: Routes = [
     path: 'todos',
     component: TodoListComponent,
     data: { title: 'Todos List' }
-  }
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    UserListComponent
+    UserListComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
